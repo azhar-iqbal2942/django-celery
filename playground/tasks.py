063402bf10f739edb67c76e7bf49b814964bc018
Azhar Iqbal <azhariqbal2942@gmail.com>
2023-07-04 01:52:18 +0500

    Worker added and dummy task updated

--- playground/tasks.py
+++ playground/tasks.py
@@ -1,8 +1,14 @@
-import time
+import random
+import requests
 from celery import shared_task
 
 
 @shared_task()
 def mail_chimp_subscribe(email):
+    # used for testing a failed api call
+    if random.choice([0, 1]):
+        raise Exception("random processing error")
+
     print(f"Subscribing email {email}")
-    time.sleep(10)
+    # used for simulating a call to a third-party api
+    requests.post("https://httpbin.org/delay/5")
